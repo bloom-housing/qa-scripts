b5a48f4364563ef0c2b01b2429fc17aed06e78fb
Sarah Keller <sarah.keller@exygy.com>
2025-08-18 17:31:04 -0400

    fix: rent range logic

--- listing_filter_checks/listings.py
+++ listing_filter_checks/listings.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import argparse
 import preset_filters as presets
+import pdb
 
 parser = argparse.ArgumentParser()
 parser.add_argument("listings_csv_path", help="file path of the listings csv export")
@@ -17,26 +18,38 @@ def printResults(df, msg=''):
     print(df['Listing Name'].unique())
 
 # TODO - research if pandas has a built in way to extract rent ranges from column
-def rentFilter(df, min_rent=None, max_rent=None):
+def rentFilter(df, d_min=None, d_max=None):
     listings = []
 
-    for i, row in rent_df.iterrows():
+    # Monthly Rent column may contain any of the following:
+    #  1. single flat rent preceded by $
+    #  2. range of flat rent values separated by -
+    #  3. single percentage value followed by % 
+    #  4. range of percentage values separated by -
+    #  5. flat rent value(s) and percentage value separated by , 
+    
+    for i, row in df.iterrows():
+        # Ignore duplicates
         if row['Listing Name'] not in listings:
-            if '%' in row['Monthly Rent']:
+            rent_string = row['Monthly Rent']
+            # Always include percentage of income listings in rent range results
+            if '%' in rent_string:
                 listings.append(row['Listing Name'])
             else:
-                rent_string = row['Monthly Rent']
+                # Extract all number values from Monthly Rent column
                 numbers_as_strings = re.findall(r'\d+', rent_string)
-                min_r_check = False
-                max_r_check = False
-                for n in numbers_as_strings:
-                    if min_rent >= int(n):
-                        min_r_check = True
-                    if max_rent <= int(n):
-                        max_r_check = True
-                    if min_r_check and max_r_check:
+
+                # For units with a range of flat rent values
+                if '-' in rent_string:
+                    a_min = int(numbers_as_strings[0])
+                    a_max = int(numbers_as_strings[1])
+                    if (a_max >= d_min and a_min <= d_max):
                         listings.append(row['Listing Name'])
-                        break
+                # For units with a single flat rent value
+                else:
+                    for n in numbers_as_strings:
+                        if d_min <= int(n) and d_max >= int(n):
+                            listings.append(row['Listing Name'])
 
     print(len(listings))
     print(listings)
@@ -86,37 +99,43 @@ bed_size_map = {
     '2': 'Two Bedroom',
     '3': 'Three Bedroom',
     '4': 'Four'
-}
+}    
 
 def build_filter():
     if filter_group == '0':
         com_select = input("Enter a community type:\n0 = disabilities\n1 = veterans\n2 = seniors 62+\n3 = homeless\n4 = families\n5 = seniors 55+\nq = quit\n")
-        com_type = community_type_map[com_select]
-        # TODO handle invalid input
-        printResults(community_filter(com_type), com_type)
+        com_type = community_type_map.get(com_select, None)
+        if com_type:
+            printResults(community_filter.get(com_type), com_type)
+        elif com_select != 'q':
+            print("Invalid selection")
     elif filter_group == '1':
         bed_size_select = input("Enter bedroom size:\n0 = studio\n1 = 1 bedroom\n2 = 2 bedroom\n3 = 3 bedroom\n4 = 4+ bedroom\nq = quit\n")
-        bed_size = bed_size_map[bed_size_select]
-        # TODO - handle invalid bed size
-        printResults(unit_filter(bed_size), bed_size)
+        bed_size = bed_size_map.get(bed_size_select, None)
+        if bed_size:
+            printResults(unit_filter(bed_size), bed_size)
+        elif bed_size_select != 'q':
+            print("Invalid selection")
     elif filter_group == '2':
         printResults(sec8(), 'Section 8')
     elif filter_group == '3':
-        rr_min = input("Enter min rent: ")
-        rr_max = input("Enter max rent: ")
-        # TODO - handle invalid input
-        min_rent = int(rr_min)
-        max_rent = int(rr_max)
-
         rent_df = unit_groups_df.loc[
             (unit_groups_df['Listing Id'].isin(active_listing_ids))
         ]
 
-        # ignore null fields
+        # Ignore null fields, listings without rent specified will not return in rent range filter
         rent_df = rent_df.dropna(subset=['Monthly Rent'])
-        rentFilter(rent_df, min_rent, max_rent)
+
+        rr_min = input("Enter min rent: ")
+        rr_max = input("Enter max rent: ")
+        try:
+            desired_min = int(rr_min)
+            desired_max = int(rr_max)
+            rentFilter(rent_df, desired_min, desired_max)
+        except:
+            print("Rent must be a number")     
     else:
-        print("invalid filter group selected")
+        print("Invalid selection")
 
 
 filter_mode = input("Enter 0 for preset filters or 1 for interactive filters\n\nEnter: ")
